a3460ed64e79afd6fbf53bbb3a29d7ee77b496b2
iShezmu <arteaga25610@gmail.com>
2024-02-28 17:13:28 +0000

    fix: typo.

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,9 +3,11 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 
-fs.readfile(filePath, 'utf8', (error, data) => {
-	if (error) {
-		return console.error(error);
+fs.readFile(filePath, 'utf8', (error, data) => {
+	if (err) {
+		console.error(err);
+		process.exit(1);
+	} else {
+		console.log(data);
 	}
-	console.log(data);
 });
