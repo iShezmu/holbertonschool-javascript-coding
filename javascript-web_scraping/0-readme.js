023d451e90ddf9bfb8ae21ca0c26f2cc48e25e8a
iShezmu <arteaga25610@gmail.com>
2024-02-28 16:49:11 +0000

    fix: codeline

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
 // a script that writes a string to a file
 const fs = require('fs');
-const fileName = process.argv[2];
-fs.readFile(fileName, 'utf8', (error, data) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(data);
-  }
+const argv = process.argv;
+
+const filePath = argv[2];
+
+fs.readfile(filePath, 'utf-8', (error, data) => {
+	if (error) {
+		console.log(error);
+		return;
+	}
+	console.log(data);
 });
