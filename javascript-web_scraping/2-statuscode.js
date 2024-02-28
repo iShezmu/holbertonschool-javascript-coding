0f99b07645d5392b35e7136b19312f21daef4fb0
iShezmu <arteaga25610@gmail.com>
2024-02-28 23:47:54 +0000

    new: script that makes a 'GET' request

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('error:', error); // Print the error if one occurred
+  } else {
+    console.log('code:', response && response.statusCode); // Print the response status code if a response was received
+  }
+});
