9c94b1e34a7a0faa1b23064587ab064157191227
iShezmu <arteaga25610@gmail.com>
2024-02-28 23:40:35 +0000

    new: fix ident

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf8', (err, data) => {
-	if (err) {
-		console.error(err);
-		process.exit(1);
-	}
-	console.log(data);
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(data);
 });
