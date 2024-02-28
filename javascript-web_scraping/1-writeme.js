60d979c2bd9dc727887f27be95e1c36a3d6ae34a
iShezmu <arteaga25610@gmail.com>
2024-02-28 23:43:58 +0000

    new: fix ident

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const fileName = process.argv[2];
-const data = process.argv[3];
+const filePath = process.argv[2];
+const content = process.argv[3];
 
-fs.writeFile(fileName, data, 'utf-8', (error) => {
-  if (error) {
-    console.error(error);
+fs.writeFile(filePath, content, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
   }
 });
