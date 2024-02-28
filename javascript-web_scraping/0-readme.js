9487f4b136f0e7de488206eb73f32c85627be95b
iShezmu <arteaga25610@gmail.com>
2024-02-28 17:10:15 +0000

    fix: code line

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
-// a script that writes a string to a file
+
 const fs = require('fs');
-const argv = process.argv;
+const filePath = process.argv[2];
 
-const filePath = argv[2];
-
-fs.readfile(filePath, 'utf-8', (error, data) => {
+fs.readfile(filePath, 'utf8', (error, data) => {
 	if (error) {
-		console.log(error);
-		return;
+		return console.error(error);
 	}
 	console.log(data);
 });
