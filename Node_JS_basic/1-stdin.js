c2473c57ed3932101ff9138664b678ccbd4c977e
iShezmu <arteaga25610@gmail.com>
2024-03-04 00:24:01 -0500

    fix: new code line & fix identation

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,9 +3,8 @@ process.stdin.setEncoding('utf8');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-  let name = process.stdin.read();
-  if (name !== null) {
-    name = name.trim(); // Trim the input to remove unexpected whitespace
+  const name = process.stdin.read();
+  if (name) {
     process.stdout.write(`Your name is: ${name}\n`); // Use \n for line feed
   }
 });
