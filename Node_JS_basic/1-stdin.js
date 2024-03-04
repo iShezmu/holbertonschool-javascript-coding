78bf902fab7d534274ed3bc3c29ce9e78cda13b2
iShezmu <arteaga25610@gmail.com>
2024-03-04 00:18:00 -0500

    new: code lines

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,15 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  
-  console.log('Welcome to Holberton School, what is your name?');
-  
-  readline.on('line', (input) => {
-    console.log(`Your name is: ${input}\r`);
-    console.log('This important software is now closing');
-    readline.close();
-  });
+process.stdin.setEncoding('utf8');
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  let name = process.stdin.read();
+  if (name !== null) {
+    name = name.trim(); // Trim the input to remove unexpected whitespace
+    process.stdout.write(`Your name is: ${name}\n`); // Use \n for line feed
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
