53945c3e94cb1a4512b7746bc0d7afc319953fda
iShezmu <arteaga25610@gmail.com>
2024-03-04 00:10:14 -0500

    fix: typo

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -6,7 +6,7 @@ const readline = require('readline').createInterface({
   console.log('Welcome to Holberton School, what is your name?');
   
   readline.on('line', (input) => {
-    console.log(`Your name is: ${input}`);
+    console.log(`Your name is: ${input}\r`);
     console.log('This important software is now closing');
     readline.close();
   });
