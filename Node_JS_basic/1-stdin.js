7eafb1b4de04f69d65e52eab14531e64d2b89b0c
iShezmu <arteaga25610@gmail.com>
2024-03-24 08:35:36 -0500

    new: returns a resolved promise with an object.

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,13 +3,12 @@ process.stdin.setEncoding('utf8');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-  let name = process.stdin.read();
-  if (name !== null) {
-    name = name.trim(); // Trim the input to remove unexpected whitespace
-    process.stdout.write(`Your name is: ${name}\n`); // Use \n for line feed
-  }
+	const name = process.stdin.read();
+	if (name) {
+		process.stdout.write(`Your name is: ${name}`);
+	}
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+	process.stdout.write('This important software is now closing\n');
 });
